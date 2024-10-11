7fe31c9e2b18ce23c4859b497f70130716056d9a
PAVAN28GIT <pavankmjp@gmail.com>
2024-10-12 03:43:32 +0530

    app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ dotenv.config({ path: './config.env' })
 
 // middlewares
 app.set("view engine" , "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,"public")));
